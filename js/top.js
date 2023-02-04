a7de68c5724b52ec627a0b847a273e23a9f1220f
sakiyamamamama <l220429@mail.ryukoku.ac.jp>
2023-02-04 16:09:09 +0900

    top

--- js/top.js
+++ js/top.js
@@ -1,12 +1,19 @@
 window.addEventListener("scroll", function () {
     const tejun = document.querySelector("#towa");
     const scroll = window.pageYOffset;
-    let k=(scroll-160)/(500-160);
+    let k=(scroll-180)/(530-180);
     tejun.style.opacity=k.toString();
   });
   window.addEventListener("scroll", function () {
     const tejun = document.querySelector("#toro");
     const scroll = window.pageYOffset;
-    let k=(scroll-700)/(1300-700);
+    let k=(scroll-810)/(1250-810);
     tejun.style.opacity=k.toString();
   });
+
+  $(document).ready(function(){
+    $(".menu p").on("click",function(){
+      $(this).next().toggleClass("hidden");
+    });
+  });
+  
