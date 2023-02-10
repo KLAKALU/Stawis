fb38088ddfd365939424043ff6d79ea192dc2502
kou <86179144+Kou0805@users.noreply.github.com>
2023-02-10 16:51:14 +0900

    ログイン、レジスターの削除

--- app.py
+++ app.py
@@ -3,19 +3,10 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-
-@app.route("register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        return "Success"
-    else:
-        return render_template("register.html")
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
 @app.route("/add", methods=["GET", "POST"])
 def add():
     return render_template("add.html")
+
 @app.route("/", methods=["GET", "POST"])
 def top():
     return render_template("top.html")
