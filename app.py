c881d9fd3254a522107ec6aecc99885f5e52ea71
kou <86179144+Kou0805@users.noreply.github.com>
2023-02-19 17:06:54 +0900

    a

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ def register():
         password = request.form.get('password')
         confirmation = request.form.get('repassword')
         username = request.form.get('username')
+        #
         user = User(email=email,username=username, password=generate_password_hash(password, method='sha256'))
         db.session.add(user)
         db.session.commit()
