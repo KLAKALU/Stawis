45a5de9c7854045c332aacd5259132ef5f7d83af
KLAKALU <player4797462226@gmail.com>
2023-04-17 15:13:44 +0900

    feat: レビューテーブルの内容を参照し合致するデータをBookテーブルから取得するクエリを追加

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ def main():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     else:
-        book = Book.query.all()
+        book = db.session.query(Book).join(Review, Book.isbn == Review.isbn).filter(Review.user_id == current_user.id)
+        print(book)
         return render_template('main.html',entries=book)
 
 #add画面
