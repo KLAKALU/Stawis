34283e5be9fef965775af94907da3ae176770178
KLAKALU <player4797462226@gmail.com>
2023-05-03 23:59:47 +0900

    add: apiを叩く関数を作成

--- getbookdetail.py
+++ getbookdetail.py
@@ -1,3 +1,16 @@
+import requests
+import json
 
-def getbookdetail():
-    
+
+def getbookdetail(apikey,isbn):
+    endpoint = 'https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404'
+    appid = '?applicationId=' + apikey
+    value = '&isbnjan=' + isbn
+    uri = endpoint + appid + value
+    r = requests.get(uri)
+    json_load = r.json()
+    print(json.dumps(json_load, indent=2, ensure_ascii=False))
+    info = {}
+    info["title"] = json_load['Items'][0]['Item']['title']
+    info['writer'] = json_load['Items'][0]['Item']['author']
+    return info
