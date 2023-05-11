62b0f21a4c9392b584880e100ecae77d00f4b2f2
KLAKALU <player4797462226@gmail.com>
2023-05-11 16:56:01 +0900

    style: コメントを加筆修正

--- app.py
+++ app.py
@@ -126,15 +126,6 @@ def googlelogin_callback():
         # Specify the CLIENT_ID of the app that accesses the backend:
         idinfo = id_token.verify_oauth2_token(request.form.get('credential'), requests.Request(), google_clientid)
 
-        # Or, if multiple clients access the backend server:
-        # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-        # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-        #     raise ValueError('Could not verify audience.')
-
-        # If auth request is from a G Suite domain:
-        # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-        #     raise ValueError('Wrong hosted domain.')
-
         # ID token is valid. Get the user's Google Account ID from the decoded token.
         userid = idinfo['sub']
         print(userid)
